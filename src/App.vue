1303fbc610dbd15a3561a274c7879c49f2e69369
gedonik <gedonik@bk.ru>
2022-06-06 12:18:35 +0300

    добавил кастомные чекбоксы сотрудникам

--- src/App.vue
+++ src/App.vue
@@ -20,41 +20,56 @@
     </div>
 
     <ul v-else class="d-flex flex-md-column mb-4">
+
       <li v-for="person in searchEmployee" :key="person.id"
           class="text-black p-4 rounded d-flex justify-content-between mb-3 shadow align-items-center">
         <strong>{{ person.name }}</strong>
         <div class="item-wrapper d-flex justify-content-between align-items-center">
           <strong>{{ person.salary.toLocaleString() }}</strong>
 
-          <div class="btns">
-            <button class="btn">
-              <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-arrow-up-square" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 9.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+          <div class="item-widgets">
+            <label class="checkbox__item checkbox-promo me-3" for="promo">
+              <input class="checkbox__input checkbox-promo__input" type="checkbox" id="promo"/>
+              <span class="checkbox__fake checkbox-promo__fake">
+              <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor"
+                   class="bi bi-arrow-up-square" viewBox="0 0 16 16">
+                <path fill-rule="evenodd"
+                      d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 9.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+              </svg>
+            </span>
+            </label>
+
+            <label class="checkbox__item checkbox-bonus me-3" for="bonus">
+              <input class="checkbox__input checkbox-bonus__input" type="checkbox" id="bonus"/>
+              <span class="checkbox__fake checkbox-bonus__fake">
+              <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-star"
+                   viewBox="0 0 16 16">
+                <path
+                    d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+              </svg>
+            </span>
+            </label>
+
+            <button @click="delEmployee(person)" class="btn delete--btn">
+              <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-trash"
+                   viewBox="0 0 16 16">
+                <path
+                    d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                <path fill-rule="evenodd"
+                      d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
               </svg>
             </button>
-          <button class="btn">
-            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
-              <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
-            </svg>
-          </button>
-          <button @click="delEmployee(person)" class="btn">
-            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-trash"
-                 viewBox="0 0 16 16">
-              <path
-                  d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-              <path fill-rule="evenodd"
-                    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-            </svg>
-          </button>
           </div>
+
         </div>
       </li>
+
     </ul>
 
     <footer class="bg-primary p-4 mb-4 rounded shadow">
       <form class="d-flex align-items-center" @submit.prevent>
-        <input class="form-control input me-4" v-model="name" type="text" placeholder="Имя сотрудника">
-        <input class="form-control input me-4" v-model="salary" type="text" placeholder="З/П сотрудника">
+        <input class="form-control footer-input me-4" v-model="name" type="text" placeholder="Имя сотрудника">
+        <input class="form-control footer-input me-4" v-model="salary" type="text" placeholder="З/П сотрудника">
         <ui-button @click="addEmployee">Добавить</ui-button>
       </form>
     </footer>
@@ -110,7 +125,75 @@ export default {
 </script>
 
 <style lang="scss">
-.btn {
+.item-wrapper {
+  max-width: 400px;
+  width: 100%;
+}
+
+.footer-input {
+  max-width: 300px;
+  width: 100%;
+}
+
+.checkbox__input {
+  width: 1px;
+  height: 1px;
+  position: absolute;
+  opacity: 0;
+  appearance: none;
+}
+
+.checkbox__item {
+  margin-left: 15px;
+  margin-bottom: 10px;
+  cursor: pointer;
+}
+
+.checkbox__fake {
+  position: relative;
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+}
+
+.checkbox-promo {
+  svg {
+    transition: fill 0.3s ease-in-out;
+
+    &:hover {
+      fill: #4fd729;
+    }
+  }
+}
+
+.checkbox-promo__input:checked + .checkbox-promo__fake {
+  svg {
+    path {
+      fill: #4fd729;
+    }
+  }
+}
+
+.checkbox-bonus {
+  svg {
+    transition: fill 0.3s ease-in-out;
+
+    &:hover {
+      fill: #f3ad2d;
+    }
+  }
+}
+
+.checkbox-bonus__input:checked + .checkbox-bonus__fake {
+  svg {
+    path {
+      fill: #f3ad2d;
+    }
+  }
+}
+
+.delete--btn {
   svg {
     transition: fill 0.3s ease-in-out;
 
@@ -120,13 +203,4 @@ export default {
   }
 }
 
-.item-wrapper {
-  max-width: 400px;
-  width: 100%;
-}
-
-.input {
-  max-width: 300px;
-  width: 100%;
-}
 </style>
